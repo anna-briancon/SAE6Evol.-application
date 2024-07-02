d652aa65929a152837e83fc2cf49fdfc996f4b7d
Anna <anna.briancon33@gmail.com>
2024-07-02 19:48:09 +0200

    modif popup reponse

--- lyrics_game/music/utils.py
+++ lyrics_game/music/utils.py
@@ -1,6 +1,7 @@
 import json
 import random
 import requests
+import re
 
 def get_lyrics(artist, title):
     url = f"https://api.lyrics.ovh/v1/{artist}/{title}"
@@ -9,18 +10,28 @@ def get_lyrics(artist, title):
         return response.json().get('lyrics', '')
     return None
 
-def get_artist():
+def clean_lyrics(lyrics, title):
+    pattern = re.compile(r"Paroles de la chanson\s" + re.escape(title) + r"\spar\s.+\n", re.IGNORECASE)
+    cleaned_lyrics = re.sub(pattern, '', lyrics)
+    return cleaned_lyrics
 
+def get_artist():
     fichier_json = './../chanteurs.json'
 
-# Lire le fichier JSON
     with open(fichier_json, 'r', encoding='utf-8') as file:
         data = json.load(file)
 
-    chanteur_aleatoire = random.choice(data )
-    
-    chanteur_modifiee = chanteur_aleatoire['name'].replace(" ", "%")
-# Afficher le contenu du fichier JSON
-    chanson_aleatoire = random.choice(chanteur_aleatoire['songs'])
-    chaine_modifiee = chanson_aleatoire.replace(" ", "%")
-    return get_lyrics(chanteur_modifiee, chaine_modifiee)
+    while True:
+        chanteur_aleatoire = random.choice(data)
+        chanteur_modifiee = chanteur_aleatoire['name'].replace(" ", "%")
+        chanson_aleatoire = random.choice(chanteur_aleatoire['songs'])
+        chaine_modifiee = chanson_aleatoire.replace(" ", "%")
+        
+        lyrics = get_lyrics(chanteur_modifiee, chaine_modifiee)
+        if lyrics:
+            cleaned_lyrics = clean_lyrics(lyrics, chanson_aleatoire)
+            return {
+                'artist': chanteur_aleatoire['name'],
+                'title': chanson_aleatoire,
+                'lyrics': cleaned_lyrics
+            }
--- lyrics_game/music/views.py
+++ lyrics_game/music/views.py
@@ -1,11 +1,14 @@
-# views.py
+from django.http import JsonResponse
 from django.shortcuts import render
-from .models import Chanteur, Chanson
-from .utils import get_lyrics, get_artist
+from .utils import get_artist
 
 def artiste_view(request):
-    lyrics = get_artist()  
-    context = {'lyrics': lyrics}
+    artist_data = get_artist()  
+    context = {
+        'lyrics': artist_data['lyrics'],
+        'correct_artist': artist_data['artist'],
+        'correct_title': artist_data['title']
+    }
     return render(request, 'artiste_page/artiste.html', context)
 
 def guess_artist(request):
@@ -13,14 +16,14 @@ def guess_artist(request):
         artist_guess = request.POST.get('artist_guess')
         title_guess = request.POST.get('title_guess')
 
-        correct_artist = 'Artiste correct'
-        correct_title = 'Titre correct'
+        correct_artist = request.POST.get('correct_artist')
+        correct_title = request.POST.get('correct_title')
+        
         if artist_guess == correct_artist and title_guess == correct_title:
             message = "Bravo, vous avez deviné correctement !"
         else:
             message = "Désolé, ce n'est pas la bonne réponse."
 
-        context = {'message': message}
-        return render(request, 'artiste_page/result.html', context)
+        return JsonResponse({'message': message})
 
-    return render(request, 'artiste_page/artiste.html')
+    return JsonResponse({'message': 'Invalid request'}, status=400)
