5a9703b485780f01a46e674c120b58a4349bc52a
Westwood <b.loupiac03@gmail.com>
2024-07-02 11:21:21 +0200

    chngment du code utils.py

--- lyrics_game/music/utils.py
+++ lyrics_game/music/utils.py
@@ -1,3 +1,5 @@
+import json
+import random
 import requests
 
 def get_lyrics(artist, title):
@@ -6,3 +8,23 @@ def get_lyrics(artist, title):
     if response.status_code == 200:
         return response.json().get('lyrics', '')
     return None
+
+def get_artist():
+
+    fichier_json = './../../chanteurs.json'
+
+# Lire le fichier JSON
+    with open(fichier_json, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    chanteur_aleatoire = random.choice(data )
+    
+    chanteur_modifiee = chanteur_aleatoire['name'].replace(" ", "%")
+# Afficher le contenu du fichier JSON
+    chanson_aleatoire = random.choice(chanteur_aleatoire['songs'])
+    chaine_modifiee = chanson_aleatoire.replace(" ", "%")
+    return get_lyrics(chanteur_modifiee, chaine_modifiee)
+
+
+    
+get_artist()     
