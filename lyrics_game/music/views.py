757a4c881fcc48f9ff07aa7ed1d32ac9702283c7
Anna <anna.briancon33@gmail.com>
2024-07-03 13:27:32 +0200

    ajout autres jeux

--- lyrics_game/music/views.py
+++ lyrics_game/music/views.py
@@ -99,3 +99,46 @@ def get_hint_title(request):
         return JsonResponse({'hint': hint, 'hint_index': hint_index})
 
     return JsonResponse({'message': 'Invalid request'}, status=400)
+
+def juste_artiste_view(request):
+    artist_data = get_artist()
+    cleaned_lyrics = clean_lyrics2(artist_data['lyrics'])
+    context = {
+        'lyrics': cleaned_lyrics,
+        'correct_artist': artist_data['artist'],
+        'correct_title': artist_data['title']
+    }
+    return render(request, 'page/juste_artiste.html', context)
+
+def guess_juste_artiste(request):
+    if request.method == 'POST':
+        artist_guess = request.POST.get('artist_guess').strip().lower()
+
+        correct_artist = request.POST.get('correct_artist').strip().lower()
+        
+        if artist_guess == correct_artist:
+            message = "Bravo, vous avez deviné correctement !"
+        else:
+            message = "Désolé, ce n'est pas la bonne réponse."
+
+        return JsonResponse({'message': message})
+
+    return JsonResponse({'message': 'Invalid request'}, status=400)
+
+def get_hint_juste_artiste(request):
+    if request.method == 'GET':
+        correct_artist = request.GET.get('correct_artist')
+        hint_type = request.GET.get('hint_type')
+        hint_index = int(request.GET.get('hint_index', 0))
+        
+        if hint_type == 'artist':
+            if hint_index < len(correct_artist):
+                hint = correct_artist[:hint_index + 1]
+                hint_index += 1
+            else:
+                hint = "Aucun autre indice disponible."
+                hint_index = len(correct_artist)
+        
+        return JsonResponse({'hint': hint, 'hint_index': hint_index})
+
+    return JsonResponse({'message': 'Invalid request'}, status=400)
